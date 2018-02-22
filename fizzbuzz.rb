98337bc49f3ef57c05a64ea7ad22eb16d2733d71
taira120 <taira1999120@gmail.com>
2018-02-22 11:57:34 +0900

    改行をなくした

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,1 @@
-# 文字数を少なくする
-(1..100).each { |i|
-  print "#{i} "
-  puts i % 3 == 0 ? i % 5 == 0 ? 'fizzbuzz' : 'fizz' : i % 5 == 0 ? 'buzz' : nil
-}
+(1..100).each { |i| print "#{i} ";puts i % 3 == 0 ? i % 5 == 0 ? 'fizzbuzz' : 'fizz' : i % 5 == 0 ? 'buzz' : nil }
