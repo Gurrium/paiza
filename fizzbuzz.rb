96cabe194f9ee87f14448c193dda79f46b9a9868
taira120 <taira1999120@gmail.com>
2018-02-22 12:44:02 +0900

    nil -> ''

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,1 @@
-(1..100).each { |i| print "#{i} ";puts i % 3 == 0 ? i % 5 == 0 ? 'fizzbuzz' : 'fizz' : i % 5 == 0 ? 'buzz' : nil }
+(1..100).each { |i| print "#{i} ";puts i % 3 == 0 ? i % 5 == 0 ? 'fizzbuzz' : 'fizz' : i % 5 == 0 ? 'buzz' : '' }
